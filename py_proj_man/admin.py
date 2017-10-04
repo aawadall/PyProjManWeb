6be53da501596d169869a2905184084660c936ee
Ahmed Awadallah <aawadall@ualberta.ca>
2017-10-04 18:19:55 +0000

    Fixing issues #1,#2, #3, #4, #5, #6, #7 and #8

--- py_proj_man/admin.py
+++ py_proj_man/admin.py
@@ -1,20 +1,29 @@
 from django.contrib import admin
 from .models import Workspace, Project, Budget, Task
-# Register your models here.
+"""PyProjMan Administration Module"""
+
 
 class TaskInline(admin.StackedInline):
-    model = Task 
+    """Tasks defined inline with Projects"""
+    model = Task
+
 
 class ProjectAdmin(admin.ModelAdmin):
+    """Project Administraion"""
     inlines = [TaskInline,]
 
+
 class ProjectInline(admin.StackedInline):
-    model = Project 
+    """Projects defined inline in a workspace"""
+    model = Project
+
 
 class WorkspaceAdmin(admin.ModelAdmin):
+    """Worjkspace administration"""
     inlines = [ProjectInline,]
  
 admin.site.register(Workspace, WorkspaceAdmin)
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Budget)
 admin.site.register(Task)
+
