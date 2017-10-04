de03c1bd8b04528a95dbe2dc891a203a083acff0
Ahmed Awadallah <aawadall@ualberta.ca>
2017-10-04 19:01:21 +0000

    fixing some issues in py_proj_man/views

--- py_proj_man/views.py
+++ py_proj_man/views.py
@@ -10,14 +10,17 @@ def default(request):
     return HttpResponse('Test Page')
 
 def workspace_detail(request, pk):
+    """Workspace View, Takes pk as an argument to lookup workspace"""
     workspace = get_object_or_404(Workspace, pk=pk)
     return render(request, 'py_proj_man/workspace_detail.html', {'workspace':workspace})
 
 def project_detail(request, ws_pk, proj_pk):
+    """Project Detailed View, takes ws_pk and proj_pk as arguments to lookup a workspace/project pair"""
     project = get_object_or_404(Project, workspace = ws_pk, pk = proj_pk)
     return render(request, 'py_proj_man/project_detail.html', {'project':project})
 
 def task_detail(request, ws_pk, proj_pk, task_pk):
+    """Task Detailed View, takes ws_pk, proj_pk and task_pk as arguments to lookup workspace/project/task tuple"""
     task = get_object_or_404(Task,  project = proj_pk, pk=task_pk)
     return render(request, 'py_proj_man/task_detail.html', {'task':task})
 
